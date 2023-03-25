f5b75ab8c484a8fb91d6dab222ea611fc25c82f6
Bill Kearney <wkearney99@users.noreply.github.com>
2023-03-25 13:06:40 -0400

    initial post
    visual basic script from homeseer to copy device name to voice command, but only for devices with voice command enabled.
    
    a thread on this: https://forums.homeseer.com/forum/homeseer-products-services/cloud-service-integrations/alexa-smart-home-skill/1600361-alexa-skill-importing-names-concatenated-with-location

---
+++ voice_rename.vb
@@ -0,0 +1,45 @@
+﻿Sub Main(ByVal Param As Object)
+
+	Dim theCount
+	theCount = 0
+
+	Dim Debug As Boolean = False
+	Dim logName As String = "Voice_command: "
+
+	Dim dv As Scheduler.Classes.DeviceClass
+	Dim EN As Scheduler.Classes.clsDeviceEnumeration
+	EN = hs.GetDeviceEnumerator
+
+	If EN Is Nothing Then
+		hs.WriteLog(logName, "Error getting Enumerator")
+		Exit Sub
+	End If
+
+	Do
+		dv = EN.GetNext
+		If dv Is Nothing Then Continue Do
+
+		Dim s
+		Dim vc
+		Dim av
+
+		s = dv.name(Nothing)
+		vc = dv.voicecommand(Nothing)
+		av = dv.MISC_Check(Nothing, Enums.dvMISC.AUTO_VOICE_COMMAND)
+
+		If (av) Then
+			If (vc = "") Then
+				hs.writelog(logName, "Name:  " & s)
+				hs.writelog(logName, "Voice: " & vc)
+				hs.writelog(logName, "Auto:  " & av)
+
+				dv.voicecommand(Nothing) = dv.name(Nothing)
+				hs.writelog(logName, "Voice command copied for: " & s)
+
+				theCount += 1
+			End If
+		End If
+	Loop Until EN.Finished
+
+	hs.writelog(logName, theCount & " voice devices detected without command.")
+End Sub
